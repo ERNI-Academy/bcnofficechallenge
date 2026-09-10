6fbf903a6c0442f23846067766ebbd0589358acf
David Carmona <davidwalker2235@gmail.com>
2026-09-10 16:00:24 +0200

    change date limit

--- features/raffle/constants.ts
+++ features/raffle/constants.ts
@@ -1,4 +1,4 @@
-export const RAFFLE_TARGET_ISO = "2026-09-09T16:22:00";
+export const RAFFLE_TARGET_ISO = "2026-09-15T20:00:00";
 
 export function isRaffleEndedAt(referenceMs: number = Date.now()): boolean {
   return referenceMs >= new Date(RAFFLE_TARGET_ISO).getTime();
